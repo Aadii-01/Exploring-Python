18f99ed50ebdc475e5edc3964541d80df52740a8
Aadii-01 <blazehere95@gmail.com>
2024-12-20 00:49:31 +0530

    New challenge completed

--- Moving on with GUI/Basics of Tkinter/main.py
+++ Moving on with GUI/Basics of Tkinter/main.py
@@ -2,28 +2,40 @@ import tkinter
 window = tkinter.Tk()  #from tkingter import Tk
 window.title("My first GUI program") #Give title
 window.minsize(width = 500, height =300) #Window size adjustments
+window.config(padx = 20, pady = 20)  #Add padding to the window
 
 """Labels"""
 my_label = tkinter.Label(text = "I am a label", font=("Times New Roman", 20, "bold"))
-my_label.pack()
-
 #Ways to change the key values
 my_label["text"] = "I am a LABEL"
 my_label.config(text = "I am a laBEL")
+my_label.config(padx= 20, pady= 20)
+my_label.pack()
+
+def get_input():
+    my_label.config(text = inputt.get() )
 
 """Buttons"""
-def button_clicked():
-    my_label.config(text = "I AM CLICKED!")
-button = tkinter.Button(text = "Click Me", command = button_clicked)
-button.pack()
-
 #Challenge -> Button clicked, changes the label text by the input text
 inputt = tkinter.Entry()
 inputt.pack()
-def get_input():
-    my_label.config(text = inputt.get() )
 button1 = tkinter.Button(text = "Enter!" , command = get_input)
 button1.pack()
+
+
+"""Challenge -> Create a screen consisiting of two buttons a label  and an entry in form of grid"""
+# my_label = tkinter.Label(text = "My label")
+# my_label.grid(column = 0, row = 0)
+#
+# b1 = tkinter.Button(text = "Click me")
+# b1.grid(column = 1, row = 1)
+#
+# b2 = tkinter.Button(text = "Dont click me")
+# b2.grid(column = 2, row = 0)
+#
+# inputt = tkinter.Entry()
+# inputt.grid(column = 3, row = 2)
+
 
 
 window.mainloop() #Keeps the window on running state
