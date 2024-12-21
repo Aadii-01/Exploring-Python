a818882fc208f153c9662b31c3797b89a43300c9
Aadii-01 <blazehere95@gmail.com>
2024-12-21 22:25:46 +0530

    Comments specified

--- Moving on with GUI/Celsius to Farenheit Converter/main.py
+++ Moving on with GUI/Celsius to Farenheit Converter/main.py
@@ -5,28 +5,32 @@ window.minsize(width = 50, height =100)
 window.config(padx = 20, pady = 20)
 
 def calculate():
-    display_farenheit.config(text = (int(enter_celsius.get()) * (9/5) + 32 ))
+    display_down.config(text = (int(enter_value.get()) * (9/5) + 32 ))
 
-
+#Equal to value
 equal_to_label = tkinter.Label(text = "is equal to")
 equal_to_label.grid(column = 0, row = 1)
 
-enter_celsius = tkinter.Entry()
-enter_celsius.config(width = 5)
-enter_celsius.grid(column = 1, row = 0)
+#Entry component for celsius
+enter_value = tkinter.Entry()
+enter_value.config(width = 5)
+enter_value.grid(column = 1, row = 0)
 
-display_farenheit = tkinter.Label(text = "0")
-display_farenheit.grid(column = 1, row = 1)
+#Farenheit label value display
+display_down = tkinter.Label(text = "0")
+display_down.grid(column = 1, row = 1)
 
-celsius_label = tkinter.Label(text = "\u00b0 Celsius")
-celsius_label.grid(column = 2, row = 0)
+#Celsius label display
+up_label = tkinter.Label(text = "\u00b0 Celsius")
+up_label.grid(column = 2, row = 0)
 
-farenheit_label = tkinter.Label(text = "\u00b0 Fahrenheit")
-farenheit_label.grid(column = 2, row = 1 )
+#Farenheit label display
+down_label = tkinter.Label(text = "\u00b0 Fahrenheit")
+down_label.grid(column = 2, row = 1 )
 
+#Click button to calculate
 calculate_button = tkinter.Button(text = "Calculate", command = calculate)
 calculate_button.grid(column = 1, row = 2)
-
 
 
 window.mainloop()
