b6ff65a8fa2a6a6928bce2d04d0ab0350ed7a41e
Aadii-01 <blazehere95@gmail.com>
2025-01-02 21:47:04 +0530

    Image centered

--- Moving on with GUI/Password Manager/main.py
+++ Moving on with GUI/Password Manager/main.py
@@ -80,7 +80,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column= 1, sticky = "w")
+canvas.grid(row=0, column= 0, columnspan = 2)
 
 #Labels
 website_label = Label(text="Website:")
